22bc384ca063feddde54de1dd510246ebffe5532
Alfredo Sánchez Alberca <asalber@gmail.com>
2020-07-28 11:14:15 +0200

    Updated

--- data-loading.R
+++ data-loading.R
@@ -1,27 +1,17 @@
 # Autor: Alfredo Sánchez Alberca (asalber@ceu.es)
 # PSA-purification
 
-# Load packages
-.packages <- c("tidyverse", "pander")
-.installed <- .packages %in% installed.packages()
-if (length(.packages[!.installed])>0) install.packages(.packages[!.installed])
-lapply(.packages, require, character.only=T)
-
-# Pander options
-panderOptions('table.alignment.default', function(df) ifelse(sapply(as.data.frame(df), is.numeric), 'right', 'left'))
-panderOptions('table.alignment.rownames', "left")
-panderOptions('keep.line.breaks', TRUE)
-
 # Data loading
-data <- read.csv(file="data/psa-data.csv", header=T, sep=",")
+data <- read.csv(file="data/data-psa.csv", header=T, sep=",", stringsAsFactors = T)
+data.recovery <- read.csv(file="data/data-recovery.csv", header=T, sep=",", stringsAsFactors = T)
 # Reorder peak levels
 data$Peak = factor(data$Peak, levels(data$Peak)[c(1, 3:10, 2)])
 
-# Data preprocessing
-data <- mutate(data, Variation = Before - After)
 # Remove NA
 data <- data[complete.cases(data), ]
 
-# Select variables
+# Filters
+data.purified <- data[data$Type == "Purified", ]
+data.non.purified <- data[data$Type == "Non-purified", ]
 
 
